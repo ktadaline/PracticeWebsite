036a763c2e3d58b4d71ed030b7529e6897b824a6
Katie Adaline Sroub <ksroub@gmail.com>
2018-07-16 12:59:16 -0400

    creating templates

--- music/views.py
+++ music/views.py
@@ -11,8 +11,11 @@ from .models import Album
 # Create your views here.
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('')
-    return HttpResponse('')
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
     #html = ''
     #for album in all_albums:
