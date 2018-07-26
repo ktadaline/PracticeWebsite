cf202109ee766ad32106239339c1fbd707a9dc58
Katie Adaline Sroub <ksroub@gmail.com>
2018-07-26 12:26:21 -0400

    make favorite option

--- music/urls.py
+++ music/urls.py
@@ -2,15 +2,19 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 
+app_name = 'music'
+
 urlpatterns = [
     # /music/
     path('', views.index, name='index'),
 
-    # /music/712/
+    # /music/<album_id>/
     path('<int:album_id>/', views.detail, name='detail'),
 
    # path('<int:album_id>', views.detail),
 
+    # /music/<album_id>/favorite/
+   # FIX path('<int:album_id>/favorite/', views.favorite, name='favorite'),
 
 
 ]
